dd0a6bb113e9e298f9f5893c7de2506bf1ac615a
Dane Johnson <daneallenjohnson@protonmail.com>
2022-02-18 15:02:51 -0600

    Better bounding boxes, use inline timer for gibs

--- baddies/Gibs.gd
+++ baddies/Gibs.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
+func _ready():
+	yield(get_tree(), "physics_frame")
+	explode()
+	yield(get_tree().create_timer(10.0), "timeout")
+	queue_free()
+
 func explode():
 	for child in get_children():
-		if child is RigidBody2D:
-			var rot = rand_range(0, PI)
-			var force = Vector2.LEFT * 10
-			force = force.rotated(rot)
-			child.apply_impulse(child.position, force)
+		var rot = rand_range(0, PI)
+		var force = Vector2.LEFT * 10
+		force = force.rotated(rot)
+		child.apply_impulse(child.position, force)
