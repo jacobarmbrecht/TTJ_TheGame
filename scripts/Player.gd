80ef0656260e8d5e9c1e49a33fcc760599a23b4b
Dane Johnson <daneallenjohnson@protonmail.com>
2022-03-01 10:51:23 -0600

    Fix do_attack timing

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,25 +6,23 @@ onready var anim_tree = $AnimationPlayer/AnimationTree
 export var speed : int = 100 #player speed
 export var jumpForce : int = 500
 export var gravity : int = 800
-var velocity : Vector2 = Vector2()  # The player's movement vector.
-var screen_size #size of window
+var velocity = Vector2.ZERO  # The player's movement vector.
 
-var is_attacking
+var is_attacking = false
 var attack_rate_done = true
 
-func _ready():
-	screen_size = get_viewport_rect().size
-	$AnimatedSprite.offset = Vector2(0,0)
-	
+onready var screen_size = get_viewport_rect().size
 
+func _ready():
+	$AnimatedSprite.offset = Vector2.ZERO
 
 func _physics_process(delta):
-	
+
 	var attack_pressed = false
 	var special_pressed = false
 	var taunt_pressed = false
-	
-	
+
+
 	# "disable" inputs when the player is attacking
 	if !is_attacking:
 		velocity.x = 0
@@ -33,16 +31,16 @@ func _physics_process(delta):
 			velocity.x += speed
 		if Input.is_action_pressed("left"):
 			velocity.x -= speed
-			
 
-		
+
+
 		#gravity
-		
-		
+
+
 		#jump
 		if Input.is_action_pressed("jump") and is_on_floor():
 			velocity.y -= jumpForce
-			
+
 		#old move and slide
 
 		#attack buttons are and if elif case
@@ -56,18 +54,17 @@ func _physics_process(delta):
 		elif Input.is_action_pressed("taunt"):
 			velocity.x = 0
 			taunt_pressed = true
-		
+
 		#choose movement anim
 		if velocity.x != 0:
 			$AnimatedSprite.animation = "walk"
 			$AnimatedSprite.flip_h = velocity.x < 0
 		else: #set the default as idle
 			$AnimatedSprite.animation = "idle"
-	
+
 		if attack_pressed and attack_rate_done:
 			$AnimatedSprite.animation = "attack"
-			do_attack();
-			#$AnimatedSprite.offset = Vector2(0,-21) #offset to deal with difference in frame size (look into using animation player instead of animated sprite)
+			do_attack()
 			is_attacking = true
 			$AttackTimer.start()
 			attack_rate_done = false
@@ -85,18 +82,17 @@ func _physics_process(delta):
 			$TauntTimer.start()
 			attack_rate_done = false
 			$AttackRate.start()
-		
+
 
 		$AnimatedSprite.play()
-		
+
 	velocity.y += gravity*delta
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
+
 
 #danes animation tree do_attack
 func do_attack():
 	anim_tree["parameters/Attack/active"] = 1
-
 
 #timer destination for ending the attack animations
 func end_attack():
