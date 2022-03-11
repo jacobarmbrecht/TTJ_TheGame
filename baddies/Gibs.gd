5ef4347cc5691ea52c7bc24df7d91d92bf86da73
Dane Johnson <daneallenjohnson@protonmail.com>
2022-03-10 19:53:15 -0600

    Fire gibs, delete old AnimatedSprite

--- baddies/Gibs.gd
+++ baddies/Gibs.gd
@@ -7,8 +7,10 @@ func _ready():
 	queue_free()
 
 func explode():
+	$CPUParticles2D.emitting = true
 	for child in get_children():
 		var rot = rand_range(0, PI)
 		var force = Vector2.LEFT * 10
 		force = force.rotated(rot)
-		child.apply_impulse(child.position, force)
+		if child is RigidBody2D:
+			child.apply_impulse(child.position, force)
