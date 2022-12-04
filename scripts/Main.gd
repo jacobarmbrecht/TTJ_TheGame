7684ceac11beed2899695a72fefbb448fb444886
armjay <45674942+jacobarmbrecht@users.noreply.github.com>
2022-12-04 16:36:50 -0600

    Added end condition on stage. Game will end when player enters the stage.

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -4,7 +4,9 @@ onready var audioplayer = $AudioStreamPlayer
 var song = preload("res://sounds/ttjvg_monday.wav")
 var endsong = preload("res://sounds/endgame/ttvg_ah.wav")
 
-onready var tileMap = $HiddenDoor
+onready var tilemap = $HiddenDoor
+onready var camera = $Player/Camera2D
+onready var endzone = $Endzone
 signal onstage
 
 export(PackedScene) var mob_scene
@@ -18,7 +20,9 @@ func _ready():
 	score = 0
 	HealthController.connect("death", self, "player_dead")
 	HealthController.connect("boss_death", self, "boss_died")
+	#endzone.connect("body_shape_entered", self, "game_over")
 	get_node("Player/Body/Sprite/Attacks/Guitar").connect("destroyed", get_node("Humuncules"), "manne_destroyed")
+	camera.limit_right = 1300
 
 func _process(delta):
 	if!audioplayer.is_playing() and !boss_died:
@@ -26,9 +30,7 @@ func _process(delta):
 		audioplayer.play()
 	
 func game_over():
-	#$MobTimer.stop()
-
-	#audioplayer.stop()
+	print("onstage")
 	emit_signal("onstage")
 	audioplayer.stream = endsong
 	audioplayer.play()
@@ -54,7 +56,8 @@ func boss_died():
 	$MobTimer.stop()
 	var index = 0
 	#still needs implemented
-	tileMap.visible = false
+	tilemap.queue_free()
+	camera.limit_right = 2176
 	audioplayer.stop()
 	#game_over()
 
@@ -65,3 +68,9 @@ func dead_mob():
 
 func player_dead():
 	pass
+
+
+
+
+func _on_Area2D_body_entered(body):
+	game_over()
